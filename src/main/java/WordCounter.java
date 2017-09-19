fc696d8ef38c33d63d22fac698288c96d48e8995
ryanestep <ryanestep@wirelessprv-10-193-111-22.near.illinois.edu>
2017-09-19 16:42:35 -0500

    Lab 3: Changed WordCounter to count how many times it says "Prince" case-insensitively in Hamlet.

---
+++ src/main/java/WordCounter.java
@@ -0,0 +1,33 @@
+import java.io.IOException;
+import java.net.URL;
+import java.util.Scanner;
+
+public class WordCounter {
+
+    public static void main(String[] args) {
+        String url = "http://erdani.com/tdpl/hamlet.txt";
+       String urlText = urlToString(url);
+//       String urlText = "prince prince a Prince string";
+        String[] urlWords = urlText.split("Hamlet");
+        int numberOfWords = urlWords.length - 1;
+        System.out.println(numberOfWords);
+    }
+
+    /**
+     * Retrieve contents from a URL and return them as a string.
+     *
+     * @param url url to retrieve contents from
+     * @return the contents from the url as a string, or an empty string on error
+     */
+    public static String urlToString(final String url) {
+        Scanner urlScanner;
+        try {
+            urlScanner = new Scanner(new URL(url).openStream(), "UTF-8");
+        } catch (IOException e) {
+            return "";
+        }
+        String contents = urlScanner.useDelimiter("\\A").next();
+        urlScanner.close();
+        return contents;
+    }
+}
